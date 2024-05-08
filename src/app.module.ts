ed4d172926a002ace77a140366d0a3e070c674a7
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-08 15:50:48 +0300

    test cors policy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './database/users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule ,UsersModule],
+  imports: [DatabaseModule ,UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
