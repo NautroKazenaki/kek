fd9f8bd5a30eb35377f976de1350d0447d93aa72
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-08 15:59:10 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as sqlite3 from 'sqlite3';
 import { UsersService } from './database/users/users.service';
-import cors from 'cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.use(cors())
    const usersService = app.get(UsersService);
    await usersService.addDefaultUserIfNeeded();
 
