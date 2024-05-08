ddab1e1cb7e3725c87ff2a9f0fe18d0a1c1b8596
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-08 15:58:24 +0300

    cors middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './database/users/users.module';
 import { DatabaseModule } from './database/database.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+
 
 @Module({
   imports: [DatabaseModule ,UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept',
+        );
+        next();
+      })
+      .forRoutes({path: '*', method: RequestMethod.ALL})
+  }
+}
