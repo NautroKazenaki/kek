45985b58fccc1f8aed843cd180dd0998d06d0d2d
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-13 14:27:49 +0300

    ip change

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { UsersService } from './database/users/users.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
-    origin:'192.168.0.*',
+    origin:'*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
    })
