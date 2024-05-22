2c3914e064198150a26f874e0402ce472913e02d
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-22 17:22:35 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import * as fs from 'fs';
 async function bootstrap() {
 
   const httpsOptions = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
   };
 
   const app = await NestFactory.create(AppModule);
