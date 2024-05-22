6f8587fa67882bb701ac1e278c38f4a4e1652c5d
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-22 17:13:31 +0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from './app.module';
 import * as sqlite3 from 'sqlite3';
 import { UsersService } from './database/users/users.service';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
 
 async function bootstrap() {
+
+  const httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+  };
+
   const app = await NestFactory.create(AppModule);
    app.enableCors({
     origin:'*',
